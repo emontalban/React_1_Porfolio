75ed175814c37f81bc12be530af2e8188f482b0f
emontalban <mentulbenodarna@gmail.com>
2026-05-20 13:19:32 +0200

    añadido el primer componente

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment/moment';
+import PortfolioContainer from './portfolio-container';
 
 export default class App extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends Component {
         <h1>DevCamp React Starter</h1>
         <h2>React Redux Router</h2>
         <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer/>
       </div>
     );
   }
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+
+export default class PortfolioContainer extends Component{
+    render() {
+        return(
+            <div>
+                <h2>Aqui estan los item del portfolio</h2>
+            </div>
+
+        );
+    }
+}
