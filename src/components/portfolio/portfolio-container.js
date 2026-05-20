8fe4da28e2f866c4746ed709e848c6b88ef4828f
emontalban <mentulbenodarna@gmail.com>
2026-05-20 20:19:04 +0200

    añadido los props

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -3,11 +3,23 @@ import PortfolioItem from "./portfolio-item";
 
 
 export default class PortfolioContainer extends Component{
+    constructor(){
+        super();
+        console.log("Portfolio container has sido renderizado")
+    }
+
+    PortfolioItem(){
+        const data = ["Google","Instagram","Facebook"];
+        return data.map(element =>{
+            return <PortfolioItem title={element} url={element +".com"}/>
+        })
+    }
     render() {
         return(
             <div>
                 <h2>Aqui estan los item del portfolio</h2>
                 <PortfolioItem/>
+                {this.PortfolioItem()}
             </div>
 
         );
