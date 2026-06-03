7528ec6626b1cb18be3900c60f2a3d1b027b060d
emontalban <mentulbenodarna@gmail.com>
2026-06-03 19:39:15 +0200

    añadida el lado derecho del portfolio sidebar

--- src/components/app.js
+++ src/components/app.js
@@ -8,9 +8,10 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import PortfolioManager from "./pages/portfolio-manager";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
-import PortfolioManager from "./pages/portfolio-manager";
+
 
 export default class App extends Component {
   constructor(props) {
--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import PortfolioSidebarList from '../portfolio/portfolio-sidebar-list';
 
 export default class PortfolioManager extends Component {
+    constructor(){
+        super();
+
+        this.state={
+            portfolioItems:[]
+        }
+    }
+
+    getPortfolioItems(){
+        axios.get('https://emontalban.devcamp.space/portfolio/portfolio_items', {withCredentials: true})
+        .then(response => {
+           this.setState({
+            portfolioItems: [...response.data.portfolio_items]
+           })
+            
+        }).catch(error =>{
+            console.log('error no responde ')
+        })
+    }
+    componentDidMount(){
+        this.getPortfolioItems();
+    }
   render() {
     return (
-      <div>
-        <h1>Hola portfolio manager</h1>
+      <div className='portfolio-manager-wrapper'>
+        <div className='left-column'>
+            <h1>Hola portfolio manager</h1>
+        </div>
+        <div className='right-column'>
+            <PortfolioSidebarList data={this.state.portfolioItems} />
+        </div>
       </div>
     );
   }
