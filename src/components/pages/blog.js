75b3a06f61bc9df68be719d3ac03e011e015ec0b
emontalban <mentulbenodarna@gmail.com>
2026-06-09 21:06:33 +0200

    añadido el scroll infinito

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,45 +1,81 @@
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import React, { Component } from 'react';
 import axios from "axios";
 import BlogItem from "../blog/blog-item";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default class Blog extends Component {
-    constructor(){
-        super();
-        
-        this.state = {
-            blogItems: []
-            };
+class Blog extends Component {
+  constructor() {
+    super();
 
-        this.getBlogItems = this.getBlogItems.bind(this);
-    }
+    this.state = {
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0, 
+      isLoading: true
+    };
 
-    getBlogItems() {
-        axios
-        .get("https://emontalban.devcamp.space/portfolio/portfolio_blogs", {
-            withCredentials: true
-        })
-        .then(response => {
-            this.setState({
-            blogItems: response.data.portfolio_blogs
-            });
-        })
-        .catch(error => {
-            console.log("getBlogItems error", error);
-        });
-    }
+    this.getBlogItems = this.getBlogItems.bind(this);
+    this.activateInfiniteScroll();
+  }
 
-    componentWillMount() {
-        this.getBlogItems();
-    }
-
-    render() {
-        
-        const blogRecords = this.state.blogItems.map(blogItem => {
-            return <BlogItem key={blogItem.id} blogItem={blogItem} />;
-        });
-
-        return <div>{blogRecords}</div>;
-
+  activateInfiniteScroll() {
+    window.onscroll = () => {
+        if(this.state.isLoading || this.state.blogItems.length === this.state.totalCount){
+            return;
         }
-    }
+    
+        if (
+        window.innerHeight + document.documentElement.scrollTop >=
+        document.documentElement.offsetHeight
+        ) {
+            this.getBlogItems();
+        console.log("get more posts");
+        }
+    };
+  }
+
+  getBlogItems() {
+    this.setState({
+        currentPage: this.state.currentPage + 1
+    })
+    axios
+      .get(`https://emontalban.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, {
+        withCredentials: true
+      })
+      .then(response => {
+        console.log("getting", response.data);
+        this.setState({
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+          totalCount : response.data.meta.total_records,
+          isLoading: false
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
+  }
+
+  render() {
+    const blogRecords = this.state.blogItems.map(blogItem => {
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
+    });
+
+    return (
+      <div className="blog-container">
+        <div className="content-container">{blogRecords}</div>
+        {this.state.isLoading ?(
+        <div className="content-loader" >
+            <FontAwesomeIcon icon="spinner" spin/>
+        </div>
+        ): null}
+      </div>
+    );
+  }
+}
+
+export default Blog;
