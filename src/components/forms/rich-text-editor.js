4921da13ff63e5ca2d3040d1c101b2c01514a2d3
emontalban <mentulbenodarna@gmail.com>
2026-06-12 12:39:35 +0200

    añadido la funcionalidad de que se abara el modal cuando quiero editar

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -7,63 +7,63 @@ import { resolve } from "path";
 import { rejects } from "assert";
 
 export default class RichTextEditor extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            editorState: EditorState.createEmpty()
-        };
-        this.onEditorStateChange = this.onEditorStateChange.bind(this);
-        this.getBase64 = this.getBase64.bind(this);
-        this.uploadFile = this.uploadFile.bind(this);
-    }
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
 
-    onEditorStateChange(editorState) {
-        this.setState(
-            { editorState },
-            this.props.handleRichTextEditorChange(
-            draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
-            )
-        );
-    }
+    this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    this.getBase64 = this.getBase64.bind(this);
+    this.uploadFile = this.uploadFile.bind(this);
+  }
 
-    uploadFile(file) {
-        return new Promise((resolve, reject) => {
-            this.getBase64(file, data => resolve({ data: { link: data } }));
-        });
-    }
+  onEditorStateChange(editorState) {
+    this.setState(
+      { editorState },
+      this.props.handleRichTextEditorChange(
+        draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+      )
+    );
+  }
 
-    getBase64(file, callback) {
-        let reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => callback(reader.result);
-        reader.onerror = error => {};
-    }
+  getBase64(file, callback) {
+    let reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => callback(reader.result);
+    reader.onerror = error => {};
+  }
 
-    render() {
-        return (
-            <div>
-            <Editor
-                editorState={this.state.editorState}
-                wrapperClassName="demo-wrapper"
-                editorClassname="demo-editor"
-                onEditorStateChange={this.onEditorStateChange}
-                toolbar={{
-                    inline: { inDropdown: true },
-                    list: { inDropdown: true },
-                    textAlign: { inDropdown: true },
-                    link: { inDropdown: true },
-                    history: { inDropdown: true },
-                    image: {
-                    uploadCallback: this.uploadFile,
-                    alt: { present: true, mandatory: false },
-                    previewImage: true,
-                    inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg"
-                    }
-                }}
-        
-            />
-            </div>
-        );
-    }
+  uploadFile(file) {
+    return new Promise((resolve, reject) => {
+      this.getBase64(file, data => resolve({ data: { link: data } }));
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Editor
+          editorState={this.state.editorState}
+          wrapperClassName="demo-wrapper"
+          editorClassname="demo-editor"
+          onEditorStateChange={this.onEditorStateChange}
+          toolbar={{
+            inline: { inDropdown: true },
+            list: { inDropdown: true },
+            textAlign: { inDropdown: true },
+            link: { inDropdown: true },
+            history: { inDropdown: true },
+            image: {
+              uploadCallback: this.uploadFile,
+              alt: { present: true, mandatory: false },
+              previewImage: true,
+              inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg"
+            }
+          }}
+        />
+      </div>
+    );
+  }
 }
--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -2,54 +2,72 @@ import React, { Component } from "react";
 import axios from "axios";
 import ReactHtmlParser from "react-html-parser";
 
+import BlogForm from "../blog/blog-form";
 import BlogFeaturedImage from "../blog/blog-featured-image";
 
 export default class BlogDetail extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-        currentId: this.props.match.params.slug,
-        blogItem: {}
-        };
-    }
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {},
+      editMode: false
+    };
 
-    getBlogItem() {
-        axios
-        .get(
-            `https://emontalban.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
-        )
-        .then(response => {
-             this.setState({
-            blogItem: response.data.portfolio_blog
+    this.handleEditClick = this.handleEditClick.bind(this);
+  }
+
+  handleEditClick() {
+    console.log("handle edit clicked");
+    this.setState({ editMode: true });
+  }
+
+  getBlogItem() {
+    axios
+      .get(
+        `https://jordan.devcamp.space/portfolio/portfolio_blogs/${
+          this.state.currentId
+        }`
+      )
+      .then(response => {
+        this.setState({
+          blogItem: response.data.portfolio_blog
         });
-        })
-        .catch(error => {
-            console.log("getBlogItem error", error);
-        });
-    }
+      })
+      .catch(error => {
+        console.log("getBlogItem error", error);
+      });
+  }
 
-    componentDidMount() {
-        this.getBlogItem();
-    }
-    render() {
-        const {
-        title,
-        content,
-        featured_image_url,
-        blog_status
-        } = this.state.blogItem;
+  componentDidMount() {
+    this.getBlogItem();
+  }
 
+  render() {
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
+
+    const contentManager = () => {
+      if (this.state.editMode) {
+        return <BlogForm />;
+      } else {
         return (
-        <div className="blog-container">
-            <div className="content-container">
-            <h1>{title}</h1>
+          <div className="content-container">
+            <h1 onClick={this.handleEditClick}>{title}</h1>
 
             <BlogFeaturedImage img={featured_image_url} />
 
             <div className="content">{ReactHtmlParser(content)}</div>
-            </div>
-        </div>
+          </div>
         );
-    }
+      }
+    };
+
+    return <div className="blog-container">{contentManager()}</div>;
+  }
 }
