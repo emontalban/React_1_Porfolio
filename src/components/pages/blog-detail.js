7e2fec98dfe47d268dbdc07557a0151e0e027441
emontalban <mentulbenodarna@gmail.com>
2026-06-12 12:54:13 +0200

    captura los datos editados del modal

--- src/components/pages/blog-detail.js
+++ src/components/pages/blog-detail.js
@@ -6,68 +6,68 @@ import BlogForm from "../blog/blog-form";
 import BlogFeaturedImage from "../blog/blog-featured-image";
 
 export default class BlogDetail extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      currentId: this.props.match.params.slug,
-      blogItem: {},
-      editMode: false
-    };
+        this.state = {
+        currentId: this.props.match.params.slug,
+        blogItem: {},
+        editMode: false
+        };
 
-    this.handleEditClick = this.handleEditClick.bind(this);
-  }
+        this.handleEditClick = this.handleEditClick.bind(this);
+    }
 
-  handleEditClick() {
-    console.log("handle edit clicked");
-    this.setState({ editMode: true });
-  }
+    handleEditClick() {
+        console.log("handle edit clicked");
+        this.setState({ editMode: true });
+    }
 
-  getBlogItem() {
-    axios
-      .get(
-        `https://jordan.devcamp.space/portfolio/portfolio_blogs/${
-          this.state.currentId
-        }`
-      )
-      .then(response => {
-        this.setState({
-          blogItem: response.data.portfolio_blog
+    getBlogItem() {
+        axios
+        .get(
+            `https://emontalban.devcamp.space/portfolio/portfolio_blogs/${
+            this.state.currentId
+            }`
+        )
+        .then(response => {
+            this.setState({
+            blogItem: response.data.portfolio_blog
+            });
+        })
+        .catch(error => {
+            console.log("getBlogItem error", error);
         });
-      })
-      .catch(error => {
-        console.log("getBlogItem error", error);
-      });
-  }
+    }
 
-  componentDidMount() {
-    this.getBlogItem();
-  }
+    componentDidMount() {
+        this.getBlogItem();
+    }
 
-  render() {
-    const {
-      title,
-      content,
-      featured_image_url,
-      blog_status
-    } = this.state.blogItem;
+    render() {
+        const {
+        title,
+        content,
+        featured_image_url,
+        blog_status
+        } = this.state.blogItem;
 
-    const contentManager = () => {
-      if (this.state.editMode) {
-        return <BlogForm />;
-      } else {
-        return (
-          <div className="content-container">
-            <h1 onClick={this.handleEditClick}>{title}</h1>
+        const contentManager = () => {
+        if (this.state.editMode) {
+            return <BlogForm editMode={this.state.editMode} blog={this.state.blogItem}/>;
+        } else {
+            return (
+            <div className="content-container">
+                <h1 onClick={this.handleEditClick}>{title}</h1>
 
-            <BlogFeaturedImage img={featured_image_url} />
+                <BlogFeaturedImage img={featured_image_url} />
 
-            <div className="content">{ReactHtmlParser(content)}</div>
-          </div>
-        );
-      }
-    };
+                <div className="content">{ReactHtmlParser(content)}</div>
+            </div>
+            );
+        }
+        };
 
-    return <div className="blog-container">{contentManager()}</div>;
-  }
+        return <div className="blog-container">{contentManager()}</div>;
+    }
 }
