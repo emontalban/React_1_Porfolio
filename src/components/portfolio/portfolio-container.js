a63efc59c8af5aa1c87c450fcc7d5fda028a0dba
emontalban <mentulbenodarna@gmail.com>
2026-06-13 12:30:18 +0200

    añadido el filtro de todos

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -4,96 +4,122 @@ import axios from "axios";
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      pageTitle: "Welcome to my portfolio",
-      isLoading: false,
-      data: []
-    };
+        this.state = {
+        pageTitle: "Welcome to my portfolio",
+        isLoading: false,
+        data: []
+        };
 
-    this.handleFilter = this.handleFilter.bind(this);
-  }
-
-  handleFilter(filter) {
-    this.setState({
-      data: this.state.data.filter(item => {
-        return filter.includes(item.category);
-      })
-    });
-  }
-
-  getPortfolioItems() {
-    axios
-      .get("https://emontalban.devcamp.space/portfolio/portfolio_items")
-      .then(response => {
-        this.setState({
-          data: response.data.portfolio_items
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
-
-  portfolioItems() {
-    return this.state.data.map(item => {
-      return <PortfolioItem key={item.id} item={item} />;
-    });
-  }
-
-  componentDidMount() {
-    this.getPortfolioItems();
-  }
-
-  render() {
-    if (this.state.isLoading) {
-      return <div>Loading...</div>;
+        this.handleFilter = this.handleFilter.bind(this);
     }
 
-    return (
-      <div className="portfolio-items-wrapper">
-        <button className="btn"
-                    onClick={() =>
-                      this.handleFilter([
-                        "Ecommerce",
-                        "Social",
-                        "Productividad",
-                        "Automatizacion"
-                      ])
-                    }
-                  >
-                    eCommerce
-                  </button>
+    handleFilter(filter) {
+        if(filter === "CLEAR_FILTERS"){
+            this.getPortfolioItems();
+        }else
+        {
+            this.getPortfolioItems(filter)
+        }
+        
+        
+    }
 
-                  <button className="btn"
-                    onClick={() =>
-                      this.handleFilter([
-                        "Eventos",
-                        "Bootcamp",
-                        "Desarrollo"])
-                    }
-                  >
-                    Scheduling
-                  </button>
+    getPortfolioItems(filter = null) {
+        axios
+        .get("https://emontalban.devcamp.space/portfolio/portfolio_items")
+        .then(response => {
+            if(filter){
+                this.setState({
+                    data: response.data.portfolio_items.filter(item => {
+                        return filter.includes(item.category);
+                    })
+                });
 
-                  <button className="btn"
-                    onClick={() =>
-                      this.handleFilter([
-                        "Educacion",
-                        "Analytics",
-                        "EdTech",
-                        "Seguridad",
-                        "Open Source"
-                      ])
-                    }
-                  >
-                    Enterprise
-                  </button>
+            }else{
+                this.setState({
+                data: response.data.portfolio_items
+                });
 
-        {this.portfolioItems()}
-      </div>
-    );
-  }
+            }
+            
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    }
+
+    portfolioItems() {
+        return this.state.data.map(item => {
+        return <PortfolioItem key={item.id} item={item} />;
+        });
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
+    render() {
+        if (this.state.isLoading) {
+        return <div>Loading...</div>;
+        }
+
+        return (
+        <div className="hompage-wrapper" >
+            <div className="filter-links">
+            <div className="portfolio-items-wrapper">
+            <button className="btn"
+                        onClick={() =>
+                            this.handleFilter([
+                            "Ecommerce",
+                            "Social",
+                            "Productividad",
+                            "Automatizacion"
+                            ])
+                        }
+                        >
+                        eCommerce
+                        </button>
+
+                        <button className="btn"
+                        onClick={() =>
+                            this.handleFilter([
+                            "Eventos",
+                            "Bootcamp",
+                            "Desarrollo"])
+                        }
+                        >
+                        Scheduling
+                        </button>
+
+                        <button className="btn"
+                        onClick={() =>
+                            this.handleFilter([
+                            "Educacion",
+                            "Analytics",
+                            "EdTech",
+                            "Seguridad",
+                            "Open Source"
+                            ])
+                        }
+                        >
+                        Enterprise
+                        </button>
+                        <button className="btn"
+                        onClick={() =>
+                            this.handleFilter(
+                            "CLEAR_FILTERS")
+                        }
+                        >
+                        All
+                        </button>
+                </div>
+                <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+            
+            </div>
+        </div>
+        );
+    }
 }
