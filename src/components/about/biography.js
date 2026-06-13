3bf19f8632b7cafedaf606d461a041375caf33fb
emontalban <mentulbenodarna@gmail.com>
2026-06-13 08:58:08 +0200

    añadida la pagina de  about

---
+++ src/components/about/biography.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Biography extends Component {
+    render() {
+        return (
+        <div>
+
+            <p></p>Soy un desarrollador de software apasionado por la creación de aplicaciones modernas, funcionales y centradas en la experiencia del usuario. Disfruto transformando ideas en soluciones digitales mediante código limpio, buenas prácticas de desarrollo y una mejora continua de mis habilidades.
+
+           <p>Mi interés abarca tanto el desarrollo frontend como backend, trabajando con tecnologías actuales para construir interfaces intuitivas, APIs eficientes y aplicaciones escalables. Considero que aprender es una parte esencial de esta profesión, por lo que siempre estoy explorando nuevas herramientas, frameworks y metodologías.</p>
+
+            <p>Me gusta afrontar nuevos retos, resolver problemas complejos y colaborar en proyectos que tengan un impacto real. Creo firmemente en la importancia del trabajo en equipo, la comunicación y la atención al detalle para conseguir productos de calidad.</p>
+
+            <p>Más allá del código, disfruto investigando nuevas tecnologías, perfeccionando mis conocimientos y desarrollando proyectos personales que me permitan experimentar y seguir creciendo como profesional. Mi objetivo es continuar evolucionando como desarrollador y contribuir a la creación de software útil, eficiente e innovador.</p>
+        </div>
+        );
+    }
+}
--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -1,5 +1,25 @@
-import React from "react";
 
-export default function() {
-  return <div>About</div>;
+import React, { Component } from 'react';
+
+import aboutImg from "../../../static/assets/images/about/about.png";
+import Biography from '../about/biography';
+
+export default class About extends Component {
+    render() {
+        return (
+        <div className="about-wrapper">
+            <div className="left-column"
+                style={{
+                    backgroundImage: `url(${aboutImg})`
+                }}/>
+
+            <div className="right-column">
+                <Biography />
+            </div>
+                    
+        
+    
+        </div>
+        );
+    }
 }
